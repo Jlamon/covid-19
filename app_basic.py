0cf17c432c0018b960bce66eb73ffaecf0cda2d7
Julien Lamon <jujuthelemon@gmail.com>
2020-12-18 22:22:06 +0100

    Initial network + some bug

--- app_basic.py
+++ app_basic.py
@@ -55,7 +55,8 @@ SIDEBAR_STYLE = {
     "bottom": 0,
     "width": "30rem",
     "padding": "2rem 1rem",
-    "background-color": "#f8f9fa"
+    "background-color": "#f8f9fa",
+    "overflow-y": "auto"
 }
 
 # the styles for the main content position it to the right of the sidebar and add some padding.
